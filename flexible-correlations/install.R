917c50261f9f9aedaf48fdea9c3976a81734d070
Dace Apšvalka <dace.apsvalka@mrc-cbu.cam.ac.uk>
2022-03-11 20:01:18 +0000

    changed packman p_load to p_install

--- flexible-correlations/install.R
+++ flexible-correlations/install.R
@@ -1,3 +1,3 @@
 install.packages("pacman")
-pacman::p_load(ggplot2, aplpack, MASS, robustbase, devtools, WRS2, cobs, robust, mgcv, scatterplot3d, quantreg, rrcov, lars, pwr, trimcluster, mc2d, psych, Rfit, DepthProc, class, fda, rankFD)
+pacman::p_install(ggplot2, aplpack, MASS, robustbase, devtools, WRS2, cobs, robust, mgcv, scatterplot3d, quantreg, rrcov, lars, pwr, trimcluster, mc2d, psych, Rfit, DepthProc, class, fda, rankFD)
 install_github("nicebread/WRS", subdir="pkg")
